2e0f08edacafb62fe8d5d179640e419bb566cdb1
martyn <martynstanley@me.com>
2020-05-31 22:39:05 +0100

    ho hum

--- golf_society/urls.py
+++ golf_society/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-from golf_app.views import IndexView
+from golf_app import views
 
 urlpatterns = [
-    url(r'^$',views.IndexView.as_view(),name='index'),
+    
     url(r'^admin/', admin.site.urls),
-    url(r'',include('golf_app.urls')),
+    url(r'^$',include('golf_app.urls')),
     url(r'accounts/login/$',views.login,name='login'),
     url(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'})
 ]
