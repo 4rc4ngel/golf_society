8b71b93502b6e2688506659d7c949f467ff27a8b
martyn <martynstanley@me.com>
2020-05-31 22:40:13 +0100

    ho hum

--- golf_society/urls.py
+++ golf_society/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-from golf_app import views
+from golf_app import urls
 
 urlpatterns = [
     
